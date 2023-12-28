a6c14ea7c1eade14ee0e81575c49df245bd16a93
明皓 刘 <1245622702@qq.com>
2023-12-28 15:15:14 +0800

    1

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ from flask import Flask, render_template
 from flask import jsonify, request, Response
 from collections import defaultdict
 import heapq
+import redis
 
 # redis_client = redis.StrictRedis(host='r-bp1t5jikzfiac5go4lpd.redis.rds.aliyuncs.com',password="wasd8456@", port=6379, db=0)
 app = Flask(__name__)
-
+redis_client = redis.StrictRedis(host='r-bp1t5jikzfiac5go4lpd.redis.rds.aliyuncs.com',password="wasd8456@", port=6379, db=0)
 from pydocumentdb import document_client
 
 # Azure Cosmos DB 连接信息
@@ -58,23 +59,32 @@ def get_reviews_data():
 
 
 @app.route('/', methods=['GET'])
-def hello_world():  # put application's code here
+def hello():  # put application's code here
+    return render_template('b.html')
+
+@app.route('/2', methods=['GET'])
+def world():  # put application's code here
     return render_template('a.html')
 
 @app.route('/closest_cities', methods=['GET'])
 def closest_cities():
+    city_name = request.args.get('city')
+    page_size = 50
+    page = int(request.args.get('page'))
+
+    # 尝试从 Redis 中获取缓存数据
+    start_redis_time = time.time()
+    cached_result = redis_client.get(f'closest_cities:{city_name}:{page}')
+    end_redis_time = time.time()
+    if cached_result:
+        # 如果缓存数据存在，直接返回
+        print("从缓存中获取数据")
+        redis_time = int((end_redis_time - start_redis_time) * 1000)  # 转换为毫秒
+        return Response(cached_result, content_type='application/json')
+
+    # 如果缓存数据不存在，执行计算和排序操作
     cities_data = get_cities_data()
     start_time = time.time()
-    city_name = request.args.get('city')
-    page_size = int(request.args.get('page_size', 50))
-    page = int(request.args.get('page', 0))
-
-    # Check if data is already in Redis
-    # redis_key = f"closest_cities:{city_name}:page{page}"
-    # redis_data = redis_client.get(redis_key)
-
-    # if redis_data:
-    #     return Response(redis_data, content_type='application/json')
 
     # Fetch data from Cosmos DB (replace this with actual Cosmos DB query)
     city_data = next((city for city in cities_data if city["city"] == city_name), None)
@@ -99,13 +109,14 @@ def closest_cities():
         end_index = len(sorted_cities)
     print(len(sorted_cities))
     paginated_result = sorted_cities[start_index:end_index]
+
     end_time = time.time()
     computing_time = int((end_time - start_time) * 1000)  # 转换为毫秒
     # Convert result to JSON format
     result_json = json.dumps({"result": paginated_result, "time_of_computing": computing_time})
 
-    # Save result to Redis
-    # redis_client.set(redis_key, result_json)
+    # 将结果缓存到 Redis 中，设置过期时间（假设设置为 1 小时）
+    redis_client.setex(f'closest_cities:{city_name}:{page}', 3600, result_json)
 
     # Return response
     return Response(result_json, content_type='application/json')
@@ -116,86 +127,7 @@ def calculate_eular_distance(x1, y1, x2, y2):
 
 
 
-# Endpoint for Q11
-with open("static/stopwords.txt", "r", encoding="utf-8") as stopwords_file:
-    stopwords = set(stopwords_file.read().splitlines())
-print(stopwords)
 
-@app.route('/knn_reviews', methods=['GET'])
-def knn_reviews():
-    reviews_data = get_reviews_data()
-    start_time = time.time()
-
-    classes = int(request.args.get('classes', 3))
-    k = int(request.args.get('k', 2))
-    words = int(request.args.get('words', 100))
-
-    # Implement KNN algorithm and process data
-    clusters = knn_algorithm(reviews_data, k, classes, words, stopwords)
-
-    # Convert clusters to JSON format
-    clusters_json = json.dumps(clusters)
-    end_time = time.time()
-    # Save result to Redis
-    # redis_key = f"knn_reviews:{k}_{classes}_{words}"
-    # redis_client.set(redis_key, clusters_json)
-
-    # 计算实际的计算时间
-    computing_time = int((end_time - start_time) * 1000)  # 转换为毫秒
-    # Return response
-    return jsonify({"result": clusters, "time_of_computing": computing_time})
-
-def knn_algorithm(reviews_data, k, classes, words, stopwords):
-    # Simplified KNN algorithm
-    clusters = defaultdict(list)
-    words_frequency = defaultdict(int)
-    city_population = defaultdict(int)
-
-    for review in reviews_data:
-        city = review["city"]
-        words_in_review = set([word.lower() for word in review["review"].split() if word.lower() not in stopwords])
-
-        # Update words frequency
-        for word in words_in_review:
-            words_frequency[word] += 1
-
-        # Update city population
-        city_population[city] += 1
-
-    # Find the k nearest neighbors for each city
-    for review in reviews_data:
-        city = review["city"]
-        words_in_review = set([word.lower() for word in review["review"].split() if word.lower() not in stopwords])
-
-        distances = []
-        for other_city in city_population:
-            if other_city != city:
-                other_city_words = set([word for word in words_in_review if word in [w["review"].lower().split() for w in reviews_data if w["city"] == other_city]])
-                distance = len(words_in_review.intersection(other_city_words))
-                heapq.heappush(distances, (distance, other_city))
-
-        # Select k nearest neighbors
-        nearest_neighbors = [city for _, city in heapq.nlargest(k, distances)]
-
-        # Add review to the cluster of the city with the nearest neighbors
-        cluster_key = frozenset(sorted([city] + nearest_neighbors))
-        clusters[cluster_key].append({"city": city, "review": review["review"]})
-
-    # Calculate the weighted average score for each cluster
-    result_clusters = []
-    for cluster_key, reviews in clusters.items():
-        center_city = list(cluster_key)[0]
-        total_weighted_score = sum(city_population[city] for city in cluster_key)
-        weighted_average_score = sum(city_population[city] * len(review["review"]) for review in reviews) / total_weighted_score
-
-        result_clusters.append({
-            "center_city": center_city,
-            "cities": list(cluster_key),
-            "words": {word: words_frequency[word] for word in words_frequency},
-            "weighted_average_score": weighted_average_score
-        })
-
-    return result_clusters
 
 if __name__ == '__main__':
     app.run()
